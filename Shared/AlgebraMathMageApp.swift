a85fe3000d21264f3be36e2d5875730f74735f7a
Andrew L. Johnson <andrewljohnson@gmail.com>
2022-08-14 19:32:46 -0700

    Initial Commit

---
+++ Shared/AlgebraMathMageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlgebraMathMageApp.swift
+//  Shared
+//
+//  Created by Andrew Johnson on 8/14/22.
+//
+
+import SwiftUI
+
+@main
+struct AlgebraMathMageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
